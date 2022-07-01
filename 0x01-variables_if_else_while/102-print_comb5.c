5f78c37567cfe98a7875cf04c88bf765fdc5dcce
Taiwo Abiodun <taiwobiodun@gmail.com>
2022-07-01 16:19:39 -0700

    program that prints all possible combinations of two digit number, range from 0 to 99, seperated by space

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,64 +1,50 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
 /**
-* main - Entry point
-* Return: Always 0
-*/
-
+ * main - main block
+ * Description: Print all possible combinations of two two-digit numbers.
+ * Numbers should range from 0 to 99.
+ * The two numbers should be separated by a space.
+ * All numbers should be printed with two digits. 1 should be printed as 01.
+ * Combination of numbers must be separated by a comma followed by a space.
+ * Combinations of numbers should be printed in ascending order.
+ * `00 01` and `01 00` are considered as the same combination.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 8 times.
+ * You are not allowed to use any variable of type `char`.
+ * Return: 0
+ */
 int main(void)
 {
-int i, j, k, l;
+	int i, j;
+	int a, b, c, d;
 
-i = j = k = 48;
-l = 49;
-while  ((i < 58))
-{
-putchar(i);
-putchar(j);
-putchar(32);
-putchar(k);
-putchar(l);
-if ((i == 57) && (j == 56) && (k == 57) && (l == 57))
-{
-putchar('\n');
-i++;
-}
-else
-{
-putchar(44);
-putchar(32);
-if ((k == 57) && (l == 57))
-{
-if (j < 56)
-{
-l = ++j + 1;
-k = i;
-}
-else if (j == 56)
-{
-j++;
-k = i + 1;
-l = 48;
-}
-else if (j == 57)
-{
-j = 48;
-l = 49;
-k = ++i;
-}
-}
-else if (l < 57)
-{
-l++;
-}
-else
-{
-l = 48;
-k++;
-}
-}
-}
-return (0);
+	for (i = 0; i < 100; i++)
+	{
+		a = i / 10; /* doubles fnum */
+		b = i % 10; /* singles fnum */
+
+		for (j = 0; j < 100; j++)
+		{
+			c = j / 10; /* doubles snum */
+			d = j % 10; /* singles snum */
+
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
 }
