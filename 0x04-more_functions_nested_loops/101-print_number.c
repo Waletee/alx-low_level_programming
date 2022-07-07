b2634c65b983b3faf04ec90e9a594e9c784ef10e
Taiwo Abiodun <taiwobiodun@gmail.com>
2022-07-07 13:17:55 -0700

    hot fix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,23 +1,31 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
-* print_number - Prints a number
-* @n: The number to print
+*main- prints the largest prime factor
+*of a number
 *
+*Return: returns 0
 */
 
-
-void print_number(int n)
+int main(void)
 {
-	unsigned int num = n;
-	if (n < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-	if (num > 9)
-	{
-		print_number(num / 10);
-	}
-	_putchar(num % 10 + '0');
+long number = 612852475143;
+int inc;
+while (inc++ < number / 2)
+{
+if (number % inc == 0)
+{
+number /= 2;
+continue;
+}
+
+for (inc = 3; inc < number / 2; inc += 2)
+{
+if (number % inc == 0)
+number /= inc;
+
+}
+}
+printf("%ld\n", number);
+return (0);
 }
