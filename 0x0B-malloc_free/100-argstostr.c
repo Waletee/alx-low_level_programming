e413592ca229ad9207270a24844b9eb6aa21f92c
waletee <taiwobiodun@gmail.com>
2022-07-25 21:10:55 +0100

    hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -20,9 +20,7 @@ char *argstostr(int ac, char **av)
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
-			len++;
-		len++;
+		for (j = 0; av[i][j] != '\0'; j++);
 	}
 
 	str = malloc(sizeof(char) * (len + 1));
@@ -37,6 +35,7 @@ char *argstostr(int ac, char **av)
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
 			str[k] = av[i][j];
+			k++;
 		}
 		str[k] = '\n';
 		k++;
